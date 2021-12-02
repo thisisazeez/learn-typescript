c17dd5b67c50239e74c7b1078be8356964e627a7
thisisazeez <imfostersyndrome@gmail.com>
2021-12-02 12:04:13 +0100

    added objects with interfaces

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+// variable declaration
+var hello = "world";
+// functions
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("abdul", "azeez"));
+var user = {
+    name: "Azeez",
+    age: 30
+};
+var user2 = {
+    name: "Jack",
+    age: 12
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,27 @@
-const a = "1";
-console.log("aaa", a);
+
+// variable declaration
+let hello:string = "world";
+
+
+// functions
+const getFullName = (name: string, surname: string) : string => {
+    return name + " " + surname;
+};
+
+console.log(getFullName("abdul", "azeez"));
+
+// objects 
+// intefaces
+interface User{
+    name: string;
+    age:number;
+}
+const user: User = {
+    name: "Azeez",
+    age:30
+};
+
+const user2 : User = {
+    name: "Jack",
+    age:12
+};
